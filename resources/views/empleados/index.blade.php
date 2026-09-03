1733f9708901b29305a35a8eff28f5ca7111c4b2
Fadventure <feredlp2008arg@gmail.com>
2026-09-03 16:56:43 -0300

    fix boton

--- resources/views/empleados/index.blade.php
+++ resources/views/empleados/index.blade.php
@@ -4,14 +4,17 @@
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Gestión de Empleados') }}
             </h2>
-            <a href="{{ route('empleados.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                + Nuevo Empleado
-            </a>
         </div>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex justify-end mb-4">
+                    <a href="{{ route('empleados.create') }}"
+                    class="bg-indigo-600 hover:bg-indigo-700 text-white text-sm font-medium px-4 py-2 rounded-md">
+                        + Nuevo empleado
+                    </a>
+                </div>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     @if(session('success'))
